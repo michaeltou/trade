baa4e848815248711fae260653c702ad3f33f7a1
moxu <joewong2000@hotmail.com>
2016-07-17 02:06:43 -0400

    trade

--- src/com/trade/tradeOrder/ITradeOrderOperation.java
+++ src/com/trade/tradeOrder/ITradeOrderOperation.java
@@ -1,11 +1,11 @@
 package com.trade.tradeOrder;
 import java.util.List;
 public interface ITradeOrderOperation {
-	    public TradeOrder selectTradeOrderByOrderID(int order_id);
+	    public TradeOrder getTradeOrderByOrderID(int order_id);
 	    
-	    public List<TradeOrder> selectTradeOrderByBuyerID(int Buyer_id);
+	    public List<TradeOrder> getTradeOrderByBuyerID(String buyer_id);
 	    
-	    public List<TradeOrder> selectTradeOrderBySellerID(int Seller_id);
+	    public List<TradeOrder> getTradeOrderBySellerID(int seller_id);
 	    
 	    public void addTradeOrder(TradeOrder tradeOrder);
 	    
--- src/com/trade/tradeOrder/TradeOrderUtil.java
+++ src/com/trade/tradeOrder/TradeOrderUtil.java
@@ -1,5 +1,7 @@
 package com.trade.tradeOrder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.util.List;
 
@@ -8,33 +10,54 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.trade.offer.Offer;
+import com.trade.offer.OfferUtil;
+
 public class TradeOrderUtil {
 
-    private static SqlSessionFactory sqlSessionFactory;
-    private static Reader reader;
+	private SqlSessionFactory wSessionFactory;
 
-    static {
-        try {
-            reader = Resources.getResourceAsReader("Configuration.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	//String resource = "conf.xml";
+    public void init(String resource) throws IOException {
+        //mybatis的配置文件
+    	if(resource == null || resource.isEmpty()) resource = "conf.xml";
+        //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+        InputStream is = OfferUtil.class.getClassLoader().getResourceAsStream(resource);
+        //构建sqlSession的工厂
+        wSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+        //Reader reader = Resources.getResourceAsReader(resource); 
+        //构建sqlSession的工厂
+        //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        //创建能执行映射文件中sql的sqlSession
+        //mSession = mSessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+       */
     }
-
-    public static SqlSessionFactory getSession() {
-        return sqlSessionFactory;
-    }
-
-    public void getTradeOrderByOrderID(int Order_id) {
-        SqlSession session = sqlSessionFactory.openSession();
+    public void getTradeOrderByOrderID(int order_id) {
+        SqlSession session = wSessionFactory.openSession();
         try {
-            ITradeOrderOperation userOperation = session
+            ITradeOrderOperation iTradeOrderOperation = session
                     .getMapper(ITradeOrderOperation.class);
-            TradeOrder user = userOperation.selectUserByID(userID);
-            if (user != null) {
-                System.out.println(user.getId() + ":" + user.getUserName()
-                        + ":" + user.getUserAddress());
+            TradeOrder tradeOrder = iTradeOrderOperation.getTradeOrderByOrderID(order_id);
+            if (tradeOrder != null) {
+                System.out.println(
+                		tradeOrder.getOrder_id() + 
+                		"buyer_id:" + tradeOrder.getBuyer_id() + 
+                		"seler_id:" + tradeOrder.getSeller_id() +
+                		"flow_status:" + tradeOrder.getFlow_status() +
+                		"pay_status:" + tradeOrder.getPay_status() +
+                		"logistics_status:" + tradeOrder.getLogistics_status() +
+                		"total_qty:" + tradeOrder.getOrder_total_qty() +
+                		"total_money:" + tradeOrder.getOrder_total_money() +
+                		"paid_money:" + tradeOrder.getOrder_total_paid_money() +
+                		"refund_id:" + tradeOrder.getOrder_refund_id() +
+                		"gmt_create:" + tradeOrder.getGmt_create() + 
+                		"gmt_modified:" + tradeOrder.getGmt_modified()    		
+                		);
             }
 
         } finally {
@@ -42,15 +65,27 @@ public class TradeOrderUtil {
         }
     }
 
-    public void getUserList(String userName) {
-        SqlSession session = sqlSessionFactory.openSession();
+    public void getTradeOrderBuyerID(String buyer_id) {
+    	  SqlSession session = wSessionFactory.openSession();
         try {
-            IUserOperation userOperation = session
-                    .getMapper(IUserOperation.class);
-            List<User> users = userOperation.selectUsersByName(userName);
-            for (User user : users) {
-                System.out.println(user.getId() + ":" + user.getUserName()
-                        + ":" + user.getUserAddress());
+            ITradeOrderOperation iTradeOrderOperation = session
+                    .getMapper(ITradeOrderOperation.class);
+            List<TradeOrder> tradeOrders = iTradeOrderOperation.getTradeOrderByBuyerID(buyer_id);
+            for (TradeOrder tradeOrder : tradeOrders) {
+                System.out.println(
+                		tradeOrder.getOrder_id() + 
+                		"buyer_id:" + tradeOrder.getBuyer_id() + 
+                		"seler_id:" + tradeOrder.getSeller_id() +
+                		"flow_status:" + tradeOrder.getFlow_status() +
+                		"pay_status:" + tradeOrder.getPay_status() +
+                		"logistics_status:" + tradeOrder.getLogistics_status() +
+                		"total_qty:" + tradeOrder.getOrder_total_qty() +
+                		"total_money:" + tradeOrder.getOrder_total_money() +
+                		"paid_money:" + tradeOrder.getOrder_total_paid_money() +
+                		"refund_id:" + tradeOrder.getOrder_refund_id() +
+                		"gmt_create:" + tradeOrder.getGmt_create() + 
+                		"gmt_modified:" + tradeOrder.getGmt_modified()    		
+                        );
             }
 
         } finally {
@@ -58,21 +93,22 @@ public class TradeOrderUtil {
         }
     }
 
+    
     /**
      * 增加后要commit
      */
-    public void addUser() {
-        User user = new User();
-        user.setUserAddress("place");
-        user.setUserName("test_add");
-        user.setUserAge(30);
-        SqlSession session = sqlSessionFactory.openSession();
+    public void addTradeOrder() {
+    	TradeOrder tradeOrder = new TradeOrder();
+    	tradeOrder.setOrder_id(1);
+    	tradeOrder.setBuyer_id(1);
+    	tradeOrder.setSeller_id(2);
+        SqlSession session = wSessionFactory.openSession();
         try {
-            IUserOperation userOperation = session
-                    .getMapper(IUserOperation.class);
-            userOperation.addUser(user);
+            ITradeOrderOperation iTradeOrderOperation = session
+                    .getMapper(ITradeOrderOperation.class);
+            iTradeOrderOperation.addTradeOrder(tradeOrder);
             session.commit();
-            System.out.println("新增用户ID：" + user.getId());
+            System.out.println("新增用户ID：" + tradeOrder.getOrder_id());
         } finally {
             session.close();
         }
@@ -81,15 +117,15 @@ public class TradeOrderUtil {
     /**
      * 修改后要commit
      */
-    public void updateUser() {
-        SqlSession session = sqlSessionFactory.openSession();
+    public void updateTradeOrder() {
+        SqlSession session = wSessionFactory.openSession();
         try {
-            IUserOperation userOperation = session
-                    .getMapper(IUserOperation.class);
-            User user = userOperation.selectUserByID(1);
-            if (user != null) {
-                user.setUserAddress("A new place");
-                userOperation.updateUser(user);
+            ITradeOrderOperation iTradeOrderOperation = session
+                    .getMapper(ITradeOrderOperation.class);
+            TradeOrder tradeOrder = iTradeOrderOperation.getTradeOrderByOrderID(1);
+            if (tradeOrder != null) {
+            	tradeOrder.setBuyer_id(10);
+            	iTradeOrderOperation.updateTradeOrder(tradeOrder);
                 session.commit();
             }
         } finally {
@@ -102,12 +138,12 @@ public class TradeOrderUtil {
      * 
      * @param id
      */
-    public void deleteUser(int id) {
-        SqlSession session = sqlSessionFactory.openSession();
+    public void deleteTradeOrder(int order_id) {
+    	SqlSession session = wSessionFactory.openSession();
         try {
-            IUserOperation userOperation = session
-                    .getMapper(IUserOperation.class);
-            userOperation.deleteUser(id);
+        	ITradeOrderOperation iTradeOrderOperation = session
+                    .getMapper(ITradeOrderOperation.class);
+        	iTradeOrderOperation.deleteTradeOrder(order_id);
             session.commit();
         } finally {
             session.close();
@@ -116,14 +152,32 @@ public class TradeOrderUtil {
 
     public static void main(String[] args) {
         try {
-            Test test = new Test();
-            // test.getUserByID(1);
-            // test.getUserList("test1");
-            // test.addUser();
-            // test.updateUser();
-            // test.deleteUser(6);
-
+        	
+        	TradeOrderUtil tradeOrderutil = new TradeOrderUtil();
+            tradeOrderutil.init("conf.xml");
+        	TradeOrder tradeOrder = new TradeOrder();
+        
+        			tradeOrder.setOrder_id(2);
+        			tradeOrder.setBuyer_id(2);
+        			tradeOrder.setSeller_id(3);
+        			tradeOrder.setFlow_status("0");
+        			tradeOrder.setPay_status("Success");
+        			tradeOrder.setLogistics_status("0");
+        			tradeOrder.setOrder_total_qty(2);
+        			tradeOrder.setOrder_total_money("10000");
+        			tradeOrder.setOrder_total_paid_money("2000");
+        			tradeOrder.setOrder_refund_id("refund");
+        			tradeOrder.setGmt_create("2016-07-10 00:00:00");
+        			tradeOrder.setGmt_modified("2016-07-09 00:00:00");
+            		//util.addOffer(offer);
+        			tradeOrderutil.getTradeOrderByOrderID(1);
+        			tradeOrderutil.getTradeOrderBuyerID("%1%");
+        			//tradeOrderutil.addTradeOrder();
+        			//tradeOrderutil.updateTradeOrder();
+        			tradeOrderutil.deleteTradeOrder(1);   
+        			 System.out.println("OK");		
         } catch (Exception e) {
+        	System.out.println("haveMessage");		
             System.out.println(e.getMessage());
         }
     }
